4c4b9bd2df9dec8a0f95a77d62915532e802522a
cvrabel <cjv38@cornell.edu>
2018-02-18 22:17:08 -0500

    Setting lineup works

--- setLineup.py
+++ setLineup.py
@@ -1,6 +1,9 @@
 # Chris Vrabel
 # 2/17/18
 
+# Script to log into ESPN Fantasy Basketball team page 
+# then set your lineup.
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -30,22 +33,35 @@ def playActives(driver):
 	for n in range(numPlayers-6, numPlayers):
 		pncSlot = "pncSlot_" + str(n)
 		if playerRowList[n].find_element_by_id(pncSlot).text == "Bench":
-			driver.find_elements_by_class_name("pncButtonMove")[n].click()
+			# Find button of player to move, and get id of that button
+			moveButton = driver.find_elements_by_class_name("pncButtonMove")[n]
+			moveButtonId = moveButton.get_attribute("id")
+			moveButtonNumber = moveButtonId[moveButtonId.index("_")+1:]
+			moveButton.click()
+
 			time.sleep(1)
+
+			# Get all buttons where the player can be moved to
 			hereButtonList = driver.find_elements_by_class_name("pncButtonHere")
+
 			for m in range(len(hereButtonList)):
+				# Get id of the destination
 				idString = hereButtonList[m].get_attribute("id")
-				playerNumber = idString[idString.index("_")+1:]
-				print(playerNumber)
+				playerNumber = int(idString[idString.index("_")+1:])
+
+				# Find game status of that player today. If no game then move player here
+				if len(gameStatusList[playerNumber].text) == 0:
+					hereButtonList[m].click()
+					break
 
 				if m == len(hereButtonList) - 1:
-					time.sleep(1)
-					print(driver.find_element_by_class_name("pncButtonMoveSelected").get_property("display"))
+					driver.find_element_by_id("pncButtonMoveSelected_" + moveButtonNumber).click()
+
 			time.sleep(1)
 
 	driver.find_element_by_id("pncSaveRoster0").click()
 
-def setLineup(username, password):
+def loginThenSetLineup(username, password):
 	chrome_options = webdriver.ChromeOptions()
 	# chrome_options.add_argument('headless')
 	driver = webdriver.Chrome(chrome_options=chrome_options)
@@ -55,8 +71,8 @@ def setLineup(username, password):
 		driver.get('http://games.espn.com/fba/signin?redir=http%3A%2F%2Fgames.espn.com%2Ffba%2Fclubhouse%3FteamId%3D1%26leagueId%3D163326%26seasonId%3D2018')
 		print("Connected to url")
 		WebDriverWait(driver, 1000).until(EC.presence_of_all_elements_located((By.XPATH,"(//iframe)")))
-		# driver.save_screenshot("screenshot.png")
 
+		# Login Page
 		login(username, password, driver)
 
 		# Team Page
@@ -69,4 +85,4 @@ def setLineup(username, password):
 
 
 if __name__ == '__main__':
-	setLineup(sys.argv[1], sys.argv[2])
+	loginThenSetLineup(sys.argv[1], sys.argv[2])
